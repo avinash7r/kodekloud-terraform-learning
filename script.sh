2b0ac2839f0f37d3e1dbdd2cefe11350e4cdc03a
Avinash Rajure <rajureavinash7@gmail.com>
2026-02-15 21:11:43 +0530

    complete script to impliment multi node k8s cluster

--- script.sh
+++ script.sh
@@ -1,9 +1,127 @@
-if [ -f "ssh_key" ]; then
-    echo "SSH key already exists. Skipping generation."
-else
-    echo "Generating SSH key..."
-    ssh-keygen -t rsa -f ssh_key 
-    chmod 400 ssh_key
+# swap-off
+swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-terraform init
+
+# forwardin IPV4
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+sudo tee /etc/sysctl.d/k8s.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Verify that the br_netfilter, overlay modules are loaded by running the following commands:
+lsmod | grep br_netfilter
+lsmod | grep overlay
+
+# Verify that the net.bridge.bridge-nf-call-iptables, net.bridge.bridge-nf-call-ip6tables, and net.ipv4.ip_forward system variables are set to 1 in your sysctl config by running the following command:
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
+
+
+
+# cintainer-d
+
+wget https://github.com/containerd/containerd/releases/download/v2.2.0/containerd-2.2.0-linux-amd64.tar.gz
+tar xvf containerd-2.2.0-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local containerd-2.2.0-linux-amd64.tar.gz
+curl -LO https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo mkdir -p /usr/local/lib/systemd/system/
+sudo mv containerd.service /usr/local/lib/systemd/system/
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
+
+# Check that containerd service is up and running
+systemctl status containerd
+
+
+
+
+# runc installation
+curl -LO https://github.com/opencontainers/runc/releases/download/v1.4.0/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+
+
+# install cni plugin
+curl -LO https://github.com/containernetworking/plugins/releases/download/v1.8.0/cni-plugins-linux-amd64-v1.8.0.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.8.0.tgz
+
+
+
+# Install kubeadm, kubelet and kubectl
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key \
+  | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' \
+  | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
+sudo apt-get update
+sudo apt-get install -y \
+  kubelet=1.34.1-1.1 \
+  kubeadm=1.34.1-1.1 \
+  kubectl=1.34.1-1.1
+
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+kubeadm version
+kubelet --version
+kubectl version --client
+
+
+
+
+#  Configure crictl to work with containerd
+sudo crictl config runtime-endpoint unix:///var/run/containerd/containerd.sock
+
+
+
+# initialize control plane
+sudo kubeadm init \
+  --pod-network-cidr=192.168.0.0/16 \
+  --apiserver-advertise-address=$(hostname -I | awk '{print $1}')  \
+  --node-name master
+
+
+#   >>>>>>> Only on master node <<<<<<<
+# after succesfull exection of this 
+
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+# calico
+# Step 1: install CRDs + operator
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.31.3/manifests/tigera-operator.yaml
+
+# Step 2: install Calico resources
+sleep 10
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.31.3/manifests/custom-resources.yaml
+
+
+
+#  temp gen token
+kubeadm token create --print-join-command
 
